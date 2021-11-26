50d54eabb8f2f55a94a00dedc016d6d1985defad
DecSP <nguyencaoson7921@gmail.com>
2021-11-26 16:22:12 +0700

    im so dumb

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ apiurl={"cf":"https://codeforces.com/api/problemset.problems",
 			"ac2":"https://kenkoooo.com/atcoder/resources/problem-models.json"
 		};
 var problem;
-var problemset;
+var problemset={};
 var choice;
 var timer;
 var timerrunning=false;
@@ -21,7 +21,7 @@ function cfget() {
 	upr.max=lwr.max=3500;
 	$.getJSON(apiurl["cf"],function(result){
 		console.log("okecf");
-		problemset=result.result;
+		problemset['cf']=result.result;
 		document.getElementById("btn").disabled=false;
 		document.getElementById("status").innerHTML="Idle";
 
@@ -33,22 +33,22 @@ function acget() {
 	upr.min=lwr.min=100;
 	upr.max=lwr.max=4400;
 	$.getJSON(apiurl["ac"],function(result){
-		problemset=result;
+		problemset['ac']=result;
 		$.getJSON(apiurl["ac2"],function(result){
 			tmp=result;
-			for (i=0;i<problemset.length;++i){
-				if (problemset[i].id in tmp){
-					tmp2=tmp[problemset[i].id].difficulty;
-					contestname=problemset[i].contest_id;
+			for (i=0;i<problemset['ac'].length;++i){
+				if (problemset['ac'][i].id in tmp){
+					tmp2=tmp[problemset['ac'][i].id].difficulty;
+					contestname=problemset['ac'][i].contest_id;
 					if (typeof(tmp2)!="number") continue;
 					if (!((contestname.substr(0,3)=="abc" && parseInt(contestname.substr(3))>47)||
 					(contestname.substr(0,3)=="arc" && parseInt(contestname.substr(3))>57) ||
 					(contestname.substr(0,3)=="agc" && parseInt(contestname.substr(3))>1) )) {
-						problemset[i].rating=0;
+						problemset['ac'][i].rating=0;
 						continue;
 					}
-					let rating=tmp[problemset[i].id].difficulty;
-					problemset[i].rating=rating;
+					let rating=tmp[problemset['ac'][i].id].difficulty;
+					problemset['ac'][i].rating=rating;
 				}
 			}
 			console.log("okeac");
@@ -62,6 +62,7 @@ function acget() {
 function getdata(){
 	choice=document.getElementById("site").value;
 	document.getElementById("btn").disabled=true;
+	console.log('disabled');
 	document.getElementById("status").innerHTML="Fetching data...";
 	if (choice=="cf") cfget();
 	else if (choice=="ac") acget();
@@ -73,41 +74,37 @@ getdata();
 function randomizingcf(){
 	rcnt=parseInt(document.getElementById("rcnt").value)
 	l=[];
-	len=(rcnt==0)?problemset.problems.length:rcnt;
+	len=(rcnt==0)?problemset['cf'].problems.length:rcnt;
 	for (i=0;i<len;++i){
-		if ("rating" in problemset.problems[i]){
-			rating=problemset.problems[i].rating;
+		if ("rating" in problemset['cf'].problems[i]){
+			rating=problemset['cf'].problems[i].rating;
 			if (rating<=upr.value&&rating>=lwr.value) l.push(i);
 		}
 	}
 	
 	nlen=l.length;
 	ridx=Math.floor(Math.random()*nlen);
-	// console.log(ridx);
-	// console.log(problemset.problems[l[ridx]]);
-	problemobj=problemset.problems[l[ridx]];
+	problemobj=problemset['cf'].problems[l[ridx]];
 	problem=problemobj.contestId+'/'+problemobj.index;
 	output = `<div class="item">Problem: <a href="${sitesurl["cf"]}${problem}" target="_blank" rel="noopener noreferrer">${problemobj.name}</a> (Rating: ${problemobj.rating})</div>`;
-	// document.getElementById("problink").innerHTML=`Problem: <a href="${sitesurl["cf"]}${problem}" target="_blank" rel="noopener noreferrer">${problemobj.name}</a> (Rating: ${problemobj.rating})`;
 	document.getElementById("problink").innerHTML+=output;
 }
 function randomizingac(){
 	rcnt=parseInt(document.getElementById("rcnt").value)
 	l=[];
-	len=problemset.length;
+	len=problemset['ac'].length;
 	for (i=0;i<len;++i){
-		if ("rating" in problemset[i]){
-			rating=problemset[i].rating;
+		if ("rating" in problemset['ac'][i]){
+			rating=problemset['ac'][i].rating;
 			if (rating<=upr.value&&rating>=lwr.value) l.push(i);
 		}
 	}
 	
 	nlen=l.length;
 	ridx=Math.floor(Math.random()*nlen);
-	problemobj=problemset[l[ridx]];
+	problemobj=problemset['ac'][l[ridx]];
 	problem=problemobj.contest_id+'/tasks/'+problemobj.id;
 	output=`<div class="item">Problem: <a href="${sitesurl["ac"]}${problem}" target="_blank" rel="noopener noreferrer">${problemobj.title}</a> (Rating: ${problemobj.rating})</div>`;
-	// document.getElementById("problink").innerHTML=`Problem: <a href="${sitesurl["ac"]}${problem}" target="_blank" rel="noopener noreferrer">${problemobj.title}</a> (Rating: ${problemobj.rating})`;
 	document.getElementById("problink").innerHTML += output;
 }
 function randomizing(){
