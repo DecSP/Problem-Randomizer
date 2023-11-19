c68c8e5289d64c76ecba63de459bc7793af46a97
Nam Nguyen Dinh <nguyennamnade22@gmail.com>
2023-11-19 15:53:15 +0700

    feat: create countdown timer (#20)

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,8 @@
-import { Avatar } from "../Avatar/Avatar";
-import { IconHelp, IconNotification } from "../icons";
 import { Icon } from "@iconify/react";
 
 export const Header = () => {
   return (
-    <header className="w-screen bg-transparent px-7 py-5 fixed top-0 z-40">
+    <header className="w-screen bg-white/60 backdrop-blur-sm px-7 py-5 fixed top-0 z-40">
       <div className="section-container text-black flex justify-end items-center">
         <div className="flex items-center">
           <a
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,9 @@ import useFetch from "../hooks/useFetch";
 import { client } from "../lib/apis";
 import { QuestionSources, QUESTIONS_SOURCES } from "../types/questions-source";
 import { Icon } from "@iconify/react";
-import { useState } from "react";
+import { useMemo, useState } from "react";
+import cx from "classnames";
+import { Counter } from "../components/Counter";
 
 const { Option } = Select;
 
@@ -32,16 +34,36 @@ interface Problem {
 }
 
 const Home: NextPage = () => {
-  const [prob, setProb] = useState<Problem>();
+  const [prob, setProb] = useState<Problem[]>([]);
+  const [timerConfig, setTimerConfig] = useState({
+    show: false,
+    minutes: 0,
+    isCounting: false,
+  });
   const { data, isLoading } = useFetch({
     api: async () => {
       const proms = [client.getCF(), client.getAC()];
       return await Promise.all(proms);
     },
   });
+  const [isTimerRunning, setIsTimerRunning] = useState<boolean>(false);
+
+  const startTimer = (minutes: number) => {
+    setTimerConfig({
+      show: true,
+      minutes,
+      isCounting: true,
+    });
+  };
 
   const onSubmit = async (values: ProblemFormFields) => {
     if (!data) return;
+
+    setTimerConfig({
+      show: false,
+      minutes: 0,
+      isCounting: false,
+    });
 
     if (values.source == "codeforces") {
       const dataCF = data[0];
@@ -80,15 +102,18 @@ const Home: NextPage = () => {
       const problem = list[Math.floor(Math.random() * list.length)];
       const CF = "https://codeforces.com/problemset/problem/";
       const problemURL = `${CF}${problem.contestId + "/" + problem.index}`;
-      setProb({
-        type: "codeforces",
-        rating: problem.rating,
-        url: problemURL,
-        name: problem.name,
-        contestName:
-          dataCF.contests.find((value: any) => value.id === problem.contestId)
-            .name ?? "Unknown contest",
-      });
+      setProb([
+        {
+          type: "codeforces",
+          rating: problem.rating,
+          url: problemURL,
+          name: problem.name,
+          contestName:
+            dataCF.contests.find((value: any) => value.id === problem.contestId)
+              .name ?? "Unknown contest",
+        },
+        ...prob,
+      ]);
     } else if (values.source === "atcoder") {
       const dataAC = data[1];
       const list = dataAC.filter(
@@ -101,168 +126,212 @@ const Home: NextPage = () => {
       const problem = list[Math.floor(Math.random() * list.length)];
       const AC = "https://atcoder.jp/contests/";
       const problemURL = `${AC}${problem.contest_id + "/tasks/" + problem.id}`;
-      setProb({
-        type: "atcoder",
-        rating: problem.rating,
-        url: problemURL,
-        name: problem.name,
-        contestName: problem.contestName,
+      setProb([
+        {
+          type: "atcoder",
+          rating: problem.rating,
+          url: problemURL,
+          name: problem.name,
+          contestName: problem.contestName,
+        },
+        ...prob,
+      ]);
+    }
+
+    if (values?.minutes && values?.minutes > 0) {
+      startTimer(values?.minutes);
+    } else {
+      setTimerConfig({
+        show: false,
+        minutes: 0,
+        isCounting: false,
       });
     }
   };
 
-  let content: JSX.Element | null = null;
-  // if (isLoading) {
-  //   content = (
-  //     <div className="w-full flex justify-center p-6">
-  //       <div className="animate-spin w-max">
-  //         <IconLoading />
-  //       </div>
-  //     </div>
-  //   );
-  // } else if (!isLoading && (!data || !data?.length)) {
-  //   content = <div className="w-full text-center p-6">No users found</div>;
-  // } else if (data?.length) {
-  //   content = (
-  //     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[42px]">
-  //       {data.map((d) => (
-  //         <UserCard key={d.id} data={d} className="!w-full" />
-  //       ))}
-  //     </div>
-  //   );
-  // }
+  let filter: JSX.Element | null = null;
 
-  content = (
-    <>
-      <Form
-        autoComplete="off"
-        initialValues={{
-          lowerDiff: DIFFICULTY_LOWER_BOUND,
-          upperDiff: DIFFICULTY_UPPER_BOUND,
-          minutes: 0,
-          recentProportion: 0,
-        }}
-        onFinish={onSubmit}
-      >
-        <Row gutter={24}>
-          <Col span={24} lg={{ span: 12 }} xl={{ span: 8 }}>
-            <Form.Item<ProblemFormFields>
-              label="Choose a site"
-              name="source"
-              rules={[
-                { required: true, message: "Please select problem source" },
-              ]}
+  filter = (
+    <Form
+      autoComplete="off"
+      initialValues={{
+        lowerDiff: DIFFICULTY_LOWER_BOUND,
+        upperDiff: DIFFICULTY_UPPER_BOUND,
+        minutes: 0,
+        recentProportion: 0,
+      }}
+      onFinish={onSubmit}
+    >
+      <Row gutter={24}>
+        <Col span={24} lg={{ span: 12 }} xl={{ span: 8 }}>
+          <Form.Item<ProblemFormFields>
+            label="Choose a site"
+            name="source"
+            rules={[
+              { required: true, message: "Please select problem source" },
+            ]}
+          >
+            <Select
+              placeholder="Choose a site"
+              allowClear={{
+                clearIcon: <Icon icon="ph:x-bold" />,
+              }}
+              suffixIcon={<Icon icon="zondicons:arrow-down" />}
             >
-              <Select
-                placeholder="Choose a site"
-                allowClear={{
-                  clearIcon: <Icon icon="ph:x-bold" />,
-                }}
-                suffixIcon={<Icon icon="zondicons:arrow-down" />}
-              >
-                {Object.keys(QUESTIONS_SOURCES).map((key) => (
-                  <Option value={key} key={key}>
-                    {QUESTIONS_SOURCES[key as QuestionSources]}
-                  </Option>
-                ))}
-              </Select>
-            </Form.Item>
-          </Col>
+              {Object.keys(QUESTIONS_SOURCES).map((key) => (
+                <Option value={key} key={key}>
+                  {QUESTIONS_SOURCES[key as QuestionSources]}
+                </Option>
+              ))}
+            </Select>
+          </Form.Item>
+        </Col>
 
-          <Col span={24} lg={{ span: 12 }} xl={{ span: 8 }}>
-            <Form.Item<ProblemFormFields>
-              label="Difficulty lower bound"
-              name="lowerDiff"
-            >
-              <Input type="number" min={DIFFICULTY_LOWER_BOUND} />
-            </Form.Item>
-          </Col>
+        <Col span={24} lg={{ span: 12 }} xl={{ span: 8 }}>
+          <Form.Item<ProblemFormFields>
+            label="Difficulty lower bound"
+            name="lowerDiff"
+          >
+            <Input
+              type="number"
+              min={DIFFICULTY_LOWER_BOUND}
+              className="!bg-transparent"
+            />
+          </Form.Item>
+        </Col>
 
-          <Col span={24} lg={{ span: 12 }} xl={{ span: 8 }}>
-            <Form.Item<ProblemFormFields>
-              label="Difficulty upper bound"
-              name="upperDiff"
-            >
-              <Input type="number" min={0} max={DIFFICULTY_UPPER_BOUND} />
-            </Form.Item>
-          </Col>
+        <Col span={24} lg={{ span: 12 }} xl={{ span: 8 }}>
+          <Form.Item<ProblemFormFields>
+            label="Difficulty upper bound"
+            name="upperDiff"
+          >
+            <Input
+              type="number"
+              min={0}
+              max={DIFFICULTY_UPPER_BOUND}
+              className="!bg-transparent"
+            />
+          </Form.Item>
+        </Col>
 
-          <Col span={24} lg={{ span: 12 }} xl={{ span: 8 }}>
-            <Form.Item<ProblemFormFields>
-              label="Timer (minutes)"
-              name="minutes"
-            >
-              <Input type="number" min={0} />
-            </Form.Item>
-          </Col>
+        <Col span={24} lg={{ span: 12 }} xl={{ span: 8 }}>
+          <Form.Item<ProblemFormFields> label="Timer (minutes)" name="minutes">
+            <Input type="number" min={0} className="!bg-transparent" />
+          </Form.Item>
+        </Col>
 
-          <Col span={24} lg={{ span: 12 }} xl={{ span: 8 }}>
-            <Form.Item<ProblemFormFields>
-              label="Number of recent problems (0 for all)"
-              name="recentProportion"
-            >
-              <Input type="number" min={0} />
-            </Form.Item>
-          </Col>
+        <Col span={24} lg={{ span: 12 }} xl={{ span: 8 }}>
+          <Form.Item<ProblemFormFields>
+            label="Number of recent problems (0 for all)"
+            name="recentProportion"
+          >
+            <Input type="number" min={0} className="!bg-transparent" />
+          </Form.Item>
+        </Col>
 
-          <Col span={24} lg={{ span: 12 }} xl={{ span: 8 }}>
-            <Form.Item<ProblemFormFields>
-              label="Users to exclude solved problems"
-              name="user"
-            >
-              <Input type="text" />
-            </Form.Item>
-          </Col>
+        <Col span={24} lg={{ span: 12 }} xl={{ span: 8 }}>
+          <Form.Item<ProblemFormFields>
+            label="Users to exclude solved problems"
+            name="user"
+          >
+            <Input type="text" className="!bg-transparent" />
+          </Form.Item>
+        </Col>
 
-          <Col span={24} lg={{ span: 12 }} xl={{ span: 8 }}>
-            <button className="form-submit-button" type="submit">
-              Submit
-            </button>
-          </Col>
-        </Row>
-      </Form>
-
-      {isLoading && (
-        <div className="w-full flex justify-center p-6">
-          <div className="animate-spin w-max">
-            <Icon icon="vaadin:spinner-third" className="text-2xl" />
-          </div>
-        </div>
-      )}
-      {prob && (
-        <Card
-          className="bg-slate-200"
-          title={`${prob.name} - Rating ${prob.rating}`}
-          extra={
-            <a className="text-gray-600" href={prob.url} target="_blank">
-              Go to problem
-            </a>
-          }
-        >
-          <p>Source: {QUESTIONS_SOURCES[prob.type]}</p>
-          <p>Contest: {prob.contestName}</p>
-        </Card>
-      )}
-    </>
+        <Col span={24} lg={{ span: 12 }} xl={{ span: 8 }}>
+          <button
+            className={cx(
+              "form-submit-button transition-opacity duration-300",
+              {
+                "!opacity-40 cursor-not-allowed": isLoading || isTimerRunning,
+              }
+            )}
+            type="submit"
+            disabled={isLoading || isTimerRunning}
+          >
+            Submit
+          </button>
+        </Col>
+      </Row>
+    </Form>
   );
 
   return (
     <div>
       <Head>
-        <title>Users</title>
+        <title>Problem Randomizer</title>
         <meta name="description" content="List user page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
 
-      <main className="bg-white min-h-screen pt-[72px]">
-        <section>
-          <div className="section-container px-10 md:px-[90px] py-[43px]">
+      <main className="relative min-h-screen pt-[72px]">
+        <div className="absolute top-0 left-0 w-screen h-full">
+          <div className="sticky top-0 w-full h-screen bg-gradient-to-b from-white via-white to-gray-300" />
+        </div>
+
+        <section className="relative">
+          <div className="section-container px-6 md:px-[90px] py-[40px]">
             <h1 className="text-2xl leading-9 mb-10">Problem Randomizer</h1>
-            {content}
+            {filter}
           </div>
         </section>
+
+        <section>
+          <div className="section-container px-6 md:px-[90px] pb-[80px]">
+            {isLoading && (
+              <div className="w-full flex justify-center p-6">
+                <div className="animate-spin w-max">
+                  <Icon icon="vaadin:spinner-third" className="text-2xl" />
+                </div>
+              </div>
+            )}
+
+            {prob.length ? (
+              <div className="flex flex-col items-stretch gap-4">
+                {prob.map((p, index) => (
+                  <Card
+                    key={p.url}
+                    className={cx("!rounded-none border-gray-200", {
+                      "bg-white": index === 0,
+                      "bg-gray-100 opacity-50 hover:bg-white hover:opacity-100":
+                        index > 0,
+                    })}
+                    title={`${p.name} - Rating ${p.rating}`}
+                    extra={
+                      <a
+                        className="text-gray-600 hover:text-gray-500"
+                        href={p.url}
+                        target="_blank"
+                      >
+                        <span className="!text-xs md:!text-sm">
+                          Go to problem
+                        </span>
+                      </a>
+                    }
+                    headStyle={{
+                      borderColor: "rgb(229, 231, 235)",
+                    }}
+                  >
+                    <p>Source: {QUESTIONS_SOURCES[p.type]}</p>
+                    <p>Contest: {p.contestName}</p>
+                  </Card>
+                ))}
+              </div>
+            ) : null}
+          </div>
+        </section>
+
+        {timerConfig.show ? (
+          <Counter
+            interval={1}
+            minutes={timerConfig.minutes}
+            className="fixed bottom-0 right-0"
+            onStart={() => setIsTimerRunning(true)}
+            onStop={() => setIsTimerRunning(false)}
+          />
+        ) : null}
       </main>
     </div>
   );
