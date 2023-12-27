8cd1abc993568dbd313c221f314c46df48e2c641
Nguyen Dinh Nam <nguyennamnade22@gmail.com>
2023-12-27 21:47:49 +0700

    feat: create ui for auth forms

---
+++ components/pages/auth/LoginForm/LoginForm.tsx
@@ -0,0 +1,88 @@
+import { Icon } from '@iconify/react'
+import { Form, Row, Col, Input } from 'antd'
+// import cx from 'classnames'
+import { useState } from 'react'
+
+export type LoginFormFields = {
+  name: string
+  password: string
+}
+
+// const { Option } = Select
+
+export const LoginForm = () => {
+  const [form] = Form.useForm()
+  const [isRevealingPassword, setIsRevealingPassword] = useState(false)
+
+  //   const revalidateDiffBound = () => {
+  //     form.validateFields(['lowerDiff', 'upperDiff'])
+  //   }
+
+  //   const diffBoundValidator = (
+  //     value: string,
+  //     left: number,
+  //     right: number,
+  //     // eslint-disable-next-line
+  //     callback: (error?: string | undefined) => void,
+  //     message: string,
+  //   ) => {
+  //     const intValue = parseInt(value)
+
+  //     if (isNaN(intValue) || intValue < left || intValue > right) {
+  //       callback(message)
+  //     } else {
+  //       callback()
+  //     }
+  //   }
+
+  return (
+    <Form
+      form={form}
+      autoComplete="off"
+      initialValues={{
+        name: '',
+        password: '',
+        retypePassword: '',
+      }}
+      //   onFinish={}
+      noValidate
+    >
+      <Row gutter={24}>
+        <Col span={24}>
+          <Form.Item<LoginFormFields> label="Username" name="name">
+            <Input type="text" className="!bg-transparent" />
+          </Form.Item>
+        </Col>
+
+        <Col span={24} className="flex gap-2 items-center">
+          <Form.Item<LoginFormFields>
+            label="Password"
+            name="password"
+            className="flex-1"
+          >
+            <Input
+              type={isRevealingPassword ? 'text' : 'password'}
+              className="!bg-transparent"
+            />
+          </Form.Item>
+          <button onClick={() => setIsRevealingPassword((open) => !open)}>
+            {isRevealingPassword ? (
+              <Icon icon="bi:eye" className="text-base" />
+            ) : (
+              <Icon icon="bi:eye-slash" className="text-base" />
+            )}
+          </button>
+        </Col>
+
+        <Col span={24}>
+          <button
+            className="form-submit-button transition-opacity duration-[250] p-0.5 hover:opacity-80 bg-gradient-to-r from-blue-500 via-blue-700 to-violet-600"
+            type="submit"
+          >
+            Log In
+          </button>
+        </Col>
+      </Row>
+    </Form>
+  )
+}
