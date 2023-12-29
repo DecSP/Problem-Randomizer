51b60594edcd8d0a83ef142251e355acd71bf5cb
Nguyen Dinh Nam <nguyennamnade22@gmail.com>
2023-12-29 17:41:33 +0700

    fix: update button component and integrate storybook

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,9 +1,12 @@
+import { Icon } from '@iconify/react'
 import cx from 'classnames'
 import React from 'react'
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: 'solid' | 'outline'
-  color?: 'black' | 'white' | 'primary'
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'solid' | 'outline' | 'text'
+  color?: 'black' | 'white' | 'primary' | 'success' | 'danger' | 'warning'
+  loading?: boolean
 }
 
 export const Button = (props: ButtonProps) => {
@@ -11,54 +14,84 @@ export const Button = (props: ButtonProps) => {
     variant = 'solid',
     color = 'primary',
     children,
+    loading,
     className,
     ...rest
   } = props
 
-  const baseClasses = 'transition-all duration-[250] p-0.5'
-  const hoverClasses =
-    variant === 'solid' && color === 'primary'
-      ? 'hover:p-1'
-      : 'hover:opacity-80'
+  const baseClasses = cx(
+    'duration-[250] h-10 flex justify-center items-center gap-2',
+    {
+      'p-0.5': variant === 'outline',
+      'py-2 px-4': variant === 'solid',
+      'p-0': variant === 'text',
+    },
+  )
 
-  let disabledClasses = ''
+  const hoverClasses = cx({
+    'transition-[padding] hover:p-1': variant === 'outline',
+    'transition-opacity hover:opacity-60': variant !== 'outline',
+  })
 
-  if (rest.disabled) {
-    if (color === 'black') {
-      disabledClasses = '!opacity-40 cursor-not-allowed'
-    } else {
-      disabledClasses = '!opacity-100 cursor-not-allowed'
-    }
-  }
+  const disabledClasses =
+    rest.disabled || loading ? '!opacity-40 cursor-not-allowed' : ''
 
-  let variantClasses =
-    'form-submit-button transition-[padding] duration-[250] p-0.5 hover:p-1 bg-gradient-to-r from-blue-500 via-blue-700 to-violet-600'
-  if (variant === 'solid') {
-    if (color === 'primary') {
-      variantClasses =
-        'form-submit-button transition-[padding] duration-[250] p-0.5 hover:p-1 bg-gradient-to-r from-blue-500 via-blue-700 to-violet-600'
-    } else {
-      variantClasses =
-        'text-white bg-black hover:bg-neutral-700 transition-colors duration-[250] px-4 py-2 border-l border-y border-neutral-600 z-30'
-    }
-  }
+  const colorClasses = cx({
+    'text-white duration-[250] bg-gradient-to-r from-blue-500 via-blue-700 to-violet-600':
+      color === 'primary' && variant !== 'text',
+    'bg-gradient-to-r from-blue-500 via-blue-700 to-violet-600 bg-clip-text':
+      color === 'primary' && variant === 'text',
+
+    'text-white bg-neutral-900': color === 'black' && variant !== 'text',
+    'text-neutral-900': color === 'black' && variant === 'text',
+
+    'text-neutral-900 bg-neutral-100 border border-neutral-200':
+      color === 'white' && variant !== 'text',
+    'text-neutral-500': color === 'white' && variant === 'text',
+
+    'text-white bg-emerald-500': color === 'success' && variant !== 'text',
+    'text-emerald-500': color === 'success' && variant === 'text',
+
+    'text-white bg-red-500': color === 'danger' && variant !== 'text',
+    'text-red-500': color === 'danger' && variant === 'text',
+
+    'text-neutral-900 bg-amber-300': color === 'warning' && variant !== 'text',
+    'text-amber-400': color === 'warning' && variant === 'text',
+  })
 
   const buttonClasses = cx(
     baseClasses,
     hoverClasses,
-    variantClasses,
+    colorClasses,
     disabledClasses,
     className,
   )
 
+  if (loading) {
+    return (
+      <button className={buttonClasses} {...rest}>
+        <Icon icon="svg-spinners:3-dots-scale" className="text-2xl" />
+      </button>
+    )
+  }
+
   return (
     <button className={buttonClasses} {...rest}>
       {variant === 'outline' ? (
-        <div className="w-full h-full flex items-center justify-center bg-white text-black transition-colors duration-[250] gap-2">
+        <div
+          className={cx(
+            'w-full h-full flex items-center justify-center bg-white text-neutral-900 gap-2 px-4',
+            {
+              'bg-emerald-50': color === 'success',
+              'bg-red-50': color === 'danger',
+              'bg-amber-50': color === 'warning',
+            },
+          )}
+        >
           {children}
         </div>
       ) : (
-        <div className="flex justify-center items-center gap-2">{children}</div>
+        children
       )}
     </button>
   )
--- components/icons/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './check-filled'
-export * from './close'
-export * from './copy'
-export * from './help'
-export * from './loading'
-export * from './nofitication'
